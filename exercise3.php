fb09e6cd9a40de3d3f0ab368146694508617ad63
Kasmoi <35563413+Kasmoi@users.noreply.github.com>
2022-02-02 10:53:42 +0200

    Update exercise3.php
    add placeholder stuff

--- exercise3.php
+++ exercise3.php
@@ -9,4 +9,34 @@
 </head>
 <body>
 <h1>exercise3</h1>
+<h2>if august</h2>
+<?php
+
+?>
+<h2></h2>
+<?php
+
+?>
+<h2></h2>
+<?php
+
+?>
+<h2></h2>
+<?php
+
+?>
+<h2></h2>
+<?php
+
+?>
+<h2></h2>
+<?php
+
+?>
+<h2></h2>
+<?php
+
+?>
+
+<a href="https://github.com/Kasmoi/kasmir_moilanen.git">link to github</a>
 </body>
